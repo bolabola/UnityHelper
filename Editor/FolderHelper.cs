e4f3fe910bd14c9ac2e11fb03fe173949e074990
bolabola <lesiry@gmail.com>
2014-12-15 16:02:03 +0800

    first

---
+++ Editor/FolderHelper.cs
@@ -0,0 +1,26 @@
+/// <summary>
+/// Create Files just by one click.
+/// </summary>
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+class FolderHelper: MonoBehaviour
+{
+		[MenuItem("Tool/FilesCreate")]
+	
+		static void CreateFolder ()
+		{
+				string[] str = {"Resources",
+									"Scripts",
+									"Prefab",
+									"Textures",
+									"Plugins",
+									"Scene"};
+				foreach (string s in str) {
+						if (!Directory.Exists (Application.dataPath + "/" + s)) {
+								Directory.CreateDirectory (Application.dataPath + "/" + s);
+						}
+				}
+				AssetDatabase.Refresh ();
+		}
+}
