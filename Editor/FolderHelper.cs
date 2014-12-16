48a2ef13107d340ea6e72eb5a237c7a7a6d8b4e5
lesiry <lesiry@gmail.com>
2014-12-16 15:22:03 +0800

    Update FolderHelper.cs

--- Editor/FolderHelper.cs
+++ Editor/FolderHelper.cs
@@ -6,22 +6,22 @@ using UnityEditor;
 using System.IO;
 class FolderHelper: MonoBehaviour
 {
-		[MenuItem("Tool/FilesCreate")]
+	[MenuItem("Tool/FilesCreate")]
 	
-		static void CreateFolder ()
-		{
-				string[] str = {"Resources",
-									"Scripts",
-									"Prefab",
-									"Textures",
-									"Plugins",
-									"Scene",
-									"Material"};
-				foreach (string s in str) {
-						if (!Directory.Exists (Application.dataPath + "/" + s)) {
-								Directory.CreateDirectory (Application.dataPath + "/" + s);
-						}
-				}
-				AssetDatabase.Refresh ();
+	static void CreateFolder ()
+	{
+		string[] str = {"Resources",
+				"Scripts",
+				"Prefab",
+				"Textures",
+				"Plugins",
+				"Scene",
+				"Material"};
+		foreach (string s in str) {
+			if (!Directory.Exists (Application.dataPath + "/" + s)) {
+				Directory.CreateDirectory (Application.dataPath + "/" + s);
+			}
 		}
+				AssetDatabase.Refresh ();
+	}
 }
