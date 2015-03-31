ba81b080c5c9a7f37cad315448874e89f6881adf
bolabola <lesiry@gmail.com>
2015-03-31 21:02:18 +0800

    fbxiportsetting

---
+++ Editor/FBXImportSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+/// <summary>
+/// FBX导入设置 第一次导入时起作用
+/// </summary>
+public class FBXImportSettings : AssetPostprocessor
+{
+
+    private void OnPreprocessModel()
+    {
+
+        ModelImporter modelImporter = assetImporter as ModelImporter;
+
+        modelImporter.animationType = ModelImporterAnimationType.None;
+        modelImporter.generateSecondaryUV = false;
+
+        Debug.Log("Importing model at: " + assetPath);
+    }
+}
